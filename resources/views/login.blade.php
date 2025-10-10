2d234c9dbd8a781cfb9ea520980bb9a0a8a3e4dd
easyPhuc <0306231143@caothang.edu.vn>
2025-10-10 22:41:58 +0700

    fix_login_page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,12 +16,13 @@
 
             <div data-mdb-input-init class="form-outline mb-4">
               <input type="email" id="form2Example18" class="form-control form-control-lg" />
-              <label class="form-label" for="form2Example18">Email address</label>
+              <label class="form-label" for="form2Example18">Email address2</label>
             </div>
 
             <div data-mdb-input-init class="form-outline mb-4">
               <input type="password" id="form2Example28" class="form-control form-control-lg" />
               <label class="form-label" for="form2Example28">Password</label>
+              
             </div>
 
             <div class="pt-1 mb-4">
