9a63d6ab4fff1911658a7e861f3637d5cf63cb36
hdanq89 <0306231101@caothang.edu.vn>
2025-10-10 08:23:33 +0700

    test

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -12,9 +12,9 @@ class ProductsController extends Controller
     public function home()
     {
 
-        // return view('home'); //home
-        $db = new DB('localhost', 'laravel', 'root', '');
-        return 'Đã gọi constructor DB!';
+        return view('home'); //home
+        // $db = new DB('localhost', 'laravel', 'root', '');
+        // return 'Đã gọi constructor DB!';
     }
 
      public function shopPage()
