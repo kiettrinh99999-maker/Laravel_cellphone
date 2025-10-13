0db0f63b333f766735e8bc4478634dc9eb623bae
Kiet <kiettrinh99999@gmail.com>
2025-10-13 16:10:29 +0700

    fix merge master

--- app/Http/Controllers/DashbroadController.php
+++ app/Http/Controllers/DashbroadController.php
@@ -10,20 +10,10 @@ class DashbroadController extends Controller
 {
     public function home()
     {
-<<<<<<< Updated upstream
-        $db = DB::getInstance(); 
-        $sql = "SELECT Image FROM products WHERE id IN (11, 12)";
-        $products = $db->getAll($sql);
-        return view('home',compact('products')); //home
-||||||| Stash base
-        return view('home'); //home
-=======
+
         $db = DB::getInstance(); 
         $sql = "SELECT Image FROM products WHERE id IN ( 112,111)";
         $products = $db->getAll($sql);
         return view('home',compact('products')); //home
->>>>>>> Stashed changes
-        // $db = new DB('localhost', 'laravel', 'root', '');
-        // return 'Đã gọi constructor DB!';
     }
 }
